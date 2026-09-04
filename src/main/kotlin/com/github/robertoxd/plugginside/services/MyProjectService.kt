aff2268303bac061586eecb62afb4bdcf83d6634
GitHub Action <action@github.com>
2026-09-04 22:07:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/robertoxd/plugginside/services/MyProjectService.kt
+++ src/main/kotlin/com/github/robertoxd/plugginside/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.robertoxd.plugginside.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.robertoxd.plugginside.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
